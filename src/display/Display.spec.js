1157d073fd90a244feecdba30756183ea9914820
MichaelBaynon <baynonmichael@gmail.com>
2019-08-19 18:39:09 -0400

    wip

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+
+import React from 'react'
+
+import Display from './Display'
+
+import renderer from 'react-test-renderer'
+
+describe('<Display />', () => {
+    it('matches snapshot', () => {
+
+        const tree = renderer.create(<Display />)
+        
+        const locked = "green-led"
+        const closed = "green-led"
+
+        const {getByText, queryByText} = render(<Display 
+        closedClass={closed}
+        lockedClass={locked}
+        />)
+
+        expect(tree.toJSON()).toMatchSnapshot()
+
+    })
+    
+})
